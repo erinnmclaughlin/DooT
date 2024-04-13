5c2ebbf918e8d51835510abf59c0e5913b513c45
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-04-13 13:03:36 -0400

    more minor cleanup

--- src/DooT/Chatbot.cs
+++ src/DooT/Chatbot.cs
@@ -4,7 +4,6 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace DooT;
